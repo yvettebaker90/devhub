3bc09fbf9f7c27f2304e52f432f4ae858740925b
sali1502 <sali1502@sudent.miun.se>
2026-03-17 18:04:19 +0100

    Add basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '**', redirectTo: '' }
+];
